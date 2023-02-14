d18c0b693120bb2540d285f0636d2240dab38a81
Aneesh Shwarma <gunukrillin@gmail.com>
2023-02-14 13:08:38 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,12 @@
+
+public class add
+{
+    
+    public static void main(String args[])
+    {
+        int a,b;
+        a=10;
+        b=5;
+        System.out.println("The sum of the numbers is "+(a+b));
+    }
+}
